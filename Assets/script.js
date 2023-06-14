8d190c5225c71054761bc03c4b94b00503196bf1
Anthony Di Popolo <ant@anthonys-mbp-3.mynetworksettings.com>
2023-06-14 17:52:29 -0400

    fixed and finished js

--- Assets/script.js
+++ Assets/script.js
@@ -48,7 +48,7 @@ $(function () {
     
   }
 
-  //
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
